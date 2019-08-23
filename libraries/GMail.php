723db7659c59d58802cc3a91322662390d00013c
Francis Ilechukwu <francis94c@gmail.com>
2019-08-23 19:27:24 +0100

    getAuthorizeURL.

--- libraries/GMail.php
+++ libraries/GMail.php
@@ -12,11 +12,25 @@ class GMail {
   private $clientId;
 
   /**
-   * [init description]
-   * @param [type] $config [description]
+   * [init Initialize library with cofigs. Can be called multiple times to set
+   *       config items]
+   * @param array $config Associative Config Array.
    */
   public function init(array $config=null):void {
     $this->clientId = $config['client_id'] ?? $this->clientId;
+  }
+  /**
+   * [getAuthorizeUrl Gets/composes the authorize url to direct users to so they
+   *                  can give your application access to their GMail accounts
+   *                  based on the given scopes.]
+   * @param  string $scope        Access Scope.
+   * @param  string $redirectUri  URL to redirect to after access is granted.
+   * @param  string $responseType Response type. 'code' by default.
+   * @return string               Authorize URL
+   */
+  public function getAuthorizeUrl(string $scope, string $redirectUri='urn:ietf:wg:oauth:2.0:oob', string $responseType='code'):string {
+    if ($scope == null) throw new Exception("GMail scope cannot be null");
+    return self::AUTH_URL."?client_id=$this->clientId&redirect_uri=$redirectUri&scope=$scope&response_type=$responseType";
   }
 }
 ?>
